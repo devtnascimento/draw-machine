5f74abf0bbe79292891caadb0317195266ff916d
Thiago Nascimento <devtnascimento@gmail.com>
2023-04-21 14:03:44 -0300

    reset do reset

---
+++ src/include/utils/movable.h
@@ -0,0 +1,16 @@
+//
+// Created by devtnascimento on 20/04/23.
+//
+
+#ifndef DRAW_MACHINE_MOVABLE_H
+#define DRAW_MACHINE_MOVABLE_H
+
+namespace primitives {
+
+    class movable {
+
+    };
+
+} // primitives
+
+#endif //DRAW_MACHINE_MOVABLE_H
---
+++ src/primitives/movable.cpp
@@ -0,0 +1,8 @@
+//
+// Created by devtnascimento on 20/04/23.
+//
+
+#include "movable.h"
+
+namespace primitives {
+} // primitives
